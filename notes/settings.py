835ed20b873ddfe8414d3f95df54b30b666c54d2
Dimonat-notavaliable <ds.shushin@gmail.com>
2023-06-13 02:36:51 +0700

    allowed vercel host

--- notes/settings.py
+++ notes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sh)u(7ga3!ltsfw5vqtc(5u&do%rvixuetj^$fbn_bf%una^=o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
